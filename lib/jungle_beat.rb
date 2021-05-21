0b9548d0fe3cf2d4eb907dbabe3bc4c373888a43
Phillip Strom <phillip.strom@gmail.com>
2021-05-21 14:44:36 -0600

    Create jungle beats wrapper

---
+++ lib/jungle_beat.rb
@@ -0,0 +1,21 @@
+require './lib/linked_list'
+
+class JungleBeat
+  attr_reader :list
+
+  def initialize
+    @list = LinkedList.new
+  end
+
+  def append(string)
+    beats = string.split(' ')
+    beats.each do |beat|
+      list.append(beat)
+    end
+    string
+  end
+
+  def count
+    list.count
+  end
+end
---
+++ test/jungle_beat_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/jungle_beat'
+
+class JungleBeatTest < Minitest::Test
+  def test_it_exists
+    jb = JungleBeat.new
+
+    assert_instance_of JungleBeat, jb
+    assert_instance_of LinkedList, jb.list
+    assert_nil jb.list.head
+  end
+
+  def test_it_can_append_multiple_beats
+    jb = JungleBeat.new
+    assert_equal "deep doo ditt", jb.append("deep doo ditt")
+    assert_equal 'deep', jb.list.head.data
+    assert_equal "doo", jb.list.head.next_node.data
+    assert_equal "woo hoo shu", jb.append("woo hoo shu")
+    assert_equal 6, jb.count
+  end
+end
